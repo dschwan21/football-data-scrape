70b33a702223c987d693bae1cb5b9696c0aab747
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-03-10 23:50:35 -0500

    trying to access schedule of nfl games

--- scrapers/nfl_data.py
+++ scrapers/nfl_data.py
@@ -4,6 +4,8 @@ import nfl_data_py as nfl
 # get list of columns to scrape
 cols = nfl.see_pbp_cols()
 
-for item in cols:
-    print(item)
+# get list of games
+print(cols[["home_team", "away_team"]])
+# for item in cols:
+#     print(item)
 # print(nfl.import_combine_data("2020-2021", "WR"))
