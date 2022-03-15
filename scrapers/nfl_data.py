a5efc7c07225090b1e0079e64475c7a80c4024b4
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-03-14 21:50:33 -0400

    pulling weekly data

--- scrapers/nfl_data.py
+++ scrapers/nfl_data.py
@@ -3,9 +3,15 @@ import nfl_data_py as nfl
 
 # get list of columns to scrape
 cols = nfl.see_pbp_cols()
-
+# gets play by play data for a 2021-2022 season
+pbp = nfl.import_pbp_data([2020, 2021], ["qb_epa", "xpass"])
 # get list of games
-print(cols[["home_team", "away_team"]])
+# print(cols)
 # for item in cols:
 #     print(item)
 # print(nfl.import_combine_data("2020-2021", "WR"))
+weekly = nfl.import_weekly_data([2021])
+
+descs = nfl.import_team_desc()
+
+print(pbp)
